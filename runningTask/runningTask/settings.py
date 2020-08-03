687fd3e78609bb5a2d00e025e3753f38bb7cafc2
ay2306 <mahajan.ayush2306@gmail.com>
2020-08-03 23:34:58 +0530

    Adding multiple features to front end This commit also resolve #11

--- runningTask/runningTask/settings.py
+++ runningTask/runningTask/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "Atlan",
         "USER": "postgres",
-        "PASSWORD": "abcd",
+        "PASSWORD": "postgres",
         "HOST": "localhost",
         "PORT": "5432",
     }
